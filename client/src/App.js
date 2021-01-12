8a9383b1e9ba27a2148056afa305fea6b27fa0e7
Artsy Biba <biba.artsiom@gmail.com>
2021-01-12 16:59:40 -0500

    adding skeleton for signin, signup and profile pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import SignIn from './pages/SignIn'; 
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 import "./App.css";
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
+      <Router>
+        <Switch>
+            <Route exact path='/'>
+              <SignIn />
+            </Route>
+            <Route exact path='/signup'>
+              <SignUp />
+            </Route>
+            <Route exact path='/profile'>
+              <Profile />
+            </Route>
+          </Switch>
+      </Router>
     </MuiThemeProvider>
   );
 }
