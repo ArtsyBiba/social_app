2a9e0436b1cb646009f749072944635af60b51b2
Artsy Biba <biba.artsiom@gmail.com>
2021-01-13 10:45:14 -0500

    express setup

--- server/app.js
+++ server/app.js
@@ -1,39 +1,15 @@
-const createError = require("http-errors");
-const express = require("express");
-const { join } = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+// const createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-const indexRouter = require("./routes/index");
-const pingRouter = require("./routes/ping");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const { json, urlencoded } = express;
+const PORT = process.env.PORT || 5000;
 
-var app = express();
-
-app.use(logger("dev"));
-app.use(json());
-app.use(urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/ping", pingRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
-});
+app.listen(PORT, () => console.log(`the server has started on port: ${PORT}`));
 
 module.exports = app;
