6a6a751e085a4046bd42ba7e717b214de0d21447
Artsy Biba <biba.artsiom@gmail.com>
2021-01-12 20:34:46 -0500

    adding signup page skeleton

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Avatar from '@material-ui/core/Avatar';
