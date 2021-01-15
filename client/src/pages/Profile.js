afd1986e327a50b74caad45dad6948c9e4c7b500
Artsy Biba <biba.artsiom@gmail.com>
2021-01-14 19:49:32 -0500

    adding logout button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,29 @@
 import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import UserContext from '../context/UserContext';
  
 export default function Profile () {
-    const { userData } = useContext(UserContext);
+    const { userData, setUserData } = useContext(UserContext);
+
+    const history = useHistory();
+
+    const logout = () => {
+        setUserData({
+            token: undefined,
+            user: undefined,
+        });
+        
+        localStorage.setItem('auth-token', '');
+
+        history.push('/');
+    };
     
     return (
         userData.user ? (
-            <div>Profile</div> 
+            <>
+                <div>Profile</div>
+                <button onClick={logout}>Log Out</button> 
+            </>
         ) : (
             <div>Unauthorized access</div>
         )
