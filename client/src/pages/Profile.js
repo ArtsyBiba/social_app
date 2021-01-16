b2a1a85503b10dd63527f2dc73fa8dcd70cd45b4
Artsy Biba <biba.artsiom@gmail.com>
2021-01-16 14:22:14 -0500

    populating navbar

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,8 @@ import UserContext from '../context/UserContext';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
 
 import Icon from '../components/Navbar/Icon';
 import UserMenu from '../components/Navbar/UserMenu';
@@ -29,6 +31,34 @@ export default function Profile () {
                             </Typography>
                         </AppName>
                         <IconsWrapper>
+                            <StyledIconButton>
+                                <Badge badgeContent={0} color='secondary'>
+                                    <Typography component='p'>
+                                        Friends
+                                    </Typography>
+                                </Badge>
+                            </StyledIconButton>
+                            <StyledIconButton>
+                                <Badge badgeContent={0} color='secondary'>
+                                    <Typography component='p'>
+                                        Friends posts
+                                    </Typography>
+                                </Badge>
+                            </StyledIconButton>
+                            <StyledIconButton>
+                                <Badge badgeContent={0} color='secondary'>
+                                    <Typography component='p'>
+                                        Opinions
+                                    </Typography>
+                                </Badge>
+                            </StyledIconButton>
+                            <StyledIconButton>
+                                <Badge badgeContent={0} color='secondary'>
+                                    <Typography component='p'>
+                                        Create Pool
+                                    </Typography>
+                                </Badge>
+                            </StyledIconButton>
                             <UserMenu />
                         </IconsWrapper>
                     </StyledToolbar>
@@ -55,8 +85,13 @@ const IconsWrapper = styled.div`
     display: flex;
     justify-content: flex-start;
     align-items: center;
+    color: white;
 `;
 
 const StyledToolbar = styled(Toolbar)`
     justify-content: space-between;
+`;
+
+const StyledIconButton = styled(IconButton)`
+    color: white;
 `;
