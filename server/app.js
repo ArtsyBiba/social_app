f628fc46d1e5125f7cbdd84378ca92770489274f
Artsy Biba <biba.artsiom@gmail.com>
2021-01-21 14:18:21 -0500

    setting up body-parser

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 // set up express
 const app = express();
-app.use(express.json());
 app.use(cors());
 
 // set up configuration for uploading files
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 const PORT = process.env.PORT || 5000;
 
