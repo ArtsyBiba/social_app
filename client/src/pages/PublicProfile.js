72002b3bfba780b79a0fb9279d2c8dee2f71a91f
Artsy Biba <biba.artsiom@gmail.com>
2021-02-10 09:23:22 -0500

    rendering avatar and user name

--- client/src/pages/PublicProfile.js
+++ client/src/pages/PublicProfile.js
@@ -8,8 +8,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar/index';
 import CreatePoll from '../components/CreatePoll/index';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
  
 export default function PublicProfile () {   
     const classes = useStyles();
@@ -27,9 +26,13 @@ export default function PublicProfile () {
                     <Sidebar /> 
                     <main className={classes.content}>
                         <div className={classes.toolbar} />
-                        <UserInputForm>
-                            {user.displayName}
-                        </UserInputForm>
+                        <UserDataWrapper>
+                            <StyledAvatar 
+                                alt={user.displayName} 
+                                src={user.avatar}
+                            />
+                            <h2>{user.displayName}</h2>
+                        </UserDataWrapper>
                     </main>
                     <CreatePoll 
                         openCreatePoll={openCreatePoll}
@@ -46,80 +49,21 @@ export default function PublicProfile () {
 const StyledPage = styled.div`
     display: flex;
     background-color: #fafafa;
-    height: 100%;
+    height: 100vh;
 `;
 
-const StyledTextField = styled(TextField)`
-    text-align: center;
-    font-size: 0.5em;
-    flex: 7;
-`;
-
-const UserInputForm = styled.form`
+const UserDataWrapper = styled.form`
     display: flex;
     flex-direction: column;
     width: 45%;
     justify-content: space-between;
+    align-items: center;
     margin: auto;
 	margin-top: 1.5em;
 `;
 
-const UserInputLine = styled.div`
-    display: flex;
-`;
-
-const SavedData = styled.div`
-    margin: auto;
-	color: grey;
-    padding-top: 10px;
-    flex: 3;
-    text-align: center;
-`;
-
-const Image = styled.img`
+const StyledAvatar = styled(Avatar)`
     height: 70px;
     width: 70px;
 `;
 
-const ImageContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    margin-top: 2em;
-`;
-
-const StyledForm = styled.div`
-    margin: auto;
-    margin-top: 0.5em;
-    margin-bottom: 0.5em;
-    width: 50%;
-`;
-
-const StyledInput = styled.input`
-    margin-top: 1em;
-`;
-
-const Prompt = styled.div`
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    flex-direction: column;
-    color: lightgray;
-    margin-top: 1.5em;
-    margin-bottom: 1em;
-`;
-
-const ImageUploader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-top: 1em;
-`;
-
-const SyledButton = styled(Button)`
-    align-self: flex-end;
-    text-transform: uppercase;
-    width: 140px;
-    margin: auto;
-    margin-top: 1.5em;
-    margin-bottom: 0;
-`;
